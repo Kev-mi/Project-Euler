bcbb7314cc98978f3086d44401cbe46cde60c570
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2021-10-27 16:55:05 +0200

    Update Project Euler #4.py

--- Project Euler #4.py
+++ Project Euler #4.py
@@ -3,14 +3,14 @@ start = time.time()
 palindrome = []
 
 
-def maxi(upper_bound):
+def palindrome product(upper_bound):
     for x in range(100, upper_bound):
         for y in range(100, x):
             if str(x*y) == str(x*y)[::-1]:
                 palindrome.append(x*y)
 
 
-maxi(1000)
+palindrome product(1000)
 print(max(palindrome))
 end = time.time()
 print(end - start)
