64ee3b9dccec15da056f883d61604e4955d61ce2
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2021-10-28 19:39:42 +0200

    Update Project Euler #31.py

--- Project Euler #31.py
+++ Project Euler #31.py
@@ -7,11 +7,11 @@ def coin_sums():
     for x in [1, 2, 5, 10, 20, 50, 100, 200]:
         for y in range(x, 201):
             ways[y] += ways[y-x]
-    print(ways[200])
+    return ways[200]
 
 
 if __name__ == "__main__":
     start = time.time()
-    coin_sums()
+    print(coin_sums())
     end = time.time()
     print(end - start)
