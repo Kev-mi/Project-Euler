d97a9c76121e4dcda59190f716eabcca8fddccca
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2021-10-28 19:38:09 +0200

    Create Project Euler #31.py

---
+++ Project Euler #31.py
@@ -0,0 +1,17 @@
+import time
+
+
+def coin_sums():
+    ways = [0] * 201
+    ways[0] = 1
+    for x in [1, 2, 5, 10, 20, 50, 100, 200]:
+        for y in range(x, 201):
+            ways[y] += ways[y-x]
+    print(ways[200])
+
+
+if __name__ == "__main__":
+    start = time.time()
+    coin_sums()
+    end = time.time()
+    print(end - start)
