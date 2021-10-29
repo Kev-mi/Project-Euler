14ff8ecc0c6bddd94896455e7eb5c5ff40631837
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2021-10-29 16:57:59 +0200

    Update Project Euler #14.cpp

--- Project Euler #14.cpp
+++ Project Euler #14.cpp
@@ -5,9 +5,9 @@ int chain = 0;
 int record = 0;
 int record_x_value = 0;
 int start_s=clock() ;
-int main()
-{
-    for (int x = 3; x < 1000000; x += 2) {
+
+void longest_collatz_sequence(int upper_bound){
+    for (int x = 3; x < upper_bound; x += 2) {
         long long y = x;
         if (chain > record) {
             record = chain;
@@ -20,6 +20,11 @@ int main()
             else {
                 y = (3 * y + 1)/2;
                 chain += 2;}}}
+}
+
+int main()
+{
+   longest_collatz_sequence(1000000);
     cout << record_x_value << endl;
     int stop_s=clock();
     cout<<(stop_s-start_s)/double(CLOCKS_PER_SEC);
