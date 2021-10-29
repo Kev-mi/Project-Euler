7f4c508c9f50d52d8e59caa902b477cab491e2b9
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2021-10-29 16:51:29 +0200

    Update Project Euler #14.cpp

--- Project Euler #14.cpp
+++ Project Euler #14.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int chain = 0;
 int record = 0;
+int record_x_value = 0;
 int start_s=clock() ;
 int main()
 {
@@ -10,7 +11,7 @@ int main()
         long long y = x;
         if (chain > record) {
             record = chain;
-            cout << x - 2 << endl;}
+            record_x_value = x;}
         chain = 0;
         while (y > 1) {
             if (y % 2 == 0) {
@@ -19,6 +20,7 @@ int main()
             else {
                 y = (3 * y + 1)/2;
                 chain += 2;}}}
+    cout << record_x_value << endl;
     int stop_s=clock();
     cout<<(stop_s-start_s)/double(CLOCKS_PER_SEC);
 }
