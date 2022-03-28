e01fc745b73a4ee8520970e4844951a7e31a8d08
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-03-28 15:59:53 +0200

    Update Project Euler #10.cpp

--- Project Euler #10.cpp
+++ Project Euler #10.cpp
@@ -9,14 +9,15 @@ std::vector<int> primelist = {2};
 void prime_finder(int number){
     int prime[number+1] ={};
     std::fill_n(prime, number+1, 1);
-    for (int y = 3;y < sqrt(number+1);y+= 2)
-    {if (prime[y] == 1)
-    {for (int x = y*y;x < number+1; x += y+y)
-    {prime[x] = 0;}}}
+    for (int y = 3;y < sqrt(number+1);y+= 2){
+        if (prime[y] == 1){
+            for (int x = y*y;x < number+1; x += y+y){
+                prime[x] = 0;}}}
     for (int x=3; x < number+1; x+=2){
-      if (prime[x] == true){primelist.push_back(x);}}
-      long long sum = std::accumulate(primelist.begin(), primelist.end(), 0LL);
-      cout << sum << endl;
+        if (prime[x] == true){
+            primelist.push_back(x);}}
+    long long sum = std::accumulate(primelist.begin(), primelist.end(), 0LL);
+    cout << sum << endl;
 }
 
 int main()
