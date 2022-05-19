ab5d9b5c918ed904af6a2769a5c367e759c36b9e
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2022-05-19 15:06:56 +0200

    Update Project Euler #3.py

--- Project Euler #3.py
+++ Project Euler #3.py
@@ -9,11 +9,17 @@ def prime_checker(number):
             number = number/2
             factor_list.append(2)
     factor_list_2 = []
-    for x in range(3, int(math.sqrt(number+1)), 2):
+    for x in range(3, math.floor(math.sqrt(number)+1), 2):
         if number % x == 0:
-            factor_list_2.append(number)
+            while number % x == 0:
+                factor_list_2.append(number)
+                factor_list.append(x)
+                number = number/x
     if len(factor_list_2) == 0:
         factor_list.append(int(number))
+        print("prime factors are ", factor_list)
+        exit()
+    if number == 1:
         print("prime factors are ", factor_list)
         exit()
     return factor_list, number
@@ -32,7 +38,7 @@ def factor_finder(prime_list, factor_list, number):
 def prime_finder(number):
     prime_list = []
     prime = [True] * (number+1)
-    for y in range(3, int(math.sqrt(number+1)), 2):
+    for y in range(3, math.floor(math.sqrt(number+1)), 2):
         if prime[y]:
             prime_list.append(y)
             for x in range(y*y, number + 1, y+y):
@@ -46,9 +52,9 @@ def prime_finder(number):
 
 if __name__ == "__main__":
     start = time.time()
-    number = 600851475143
+    number = 19
     factor_list, number = prime_checker(number)
-    prime_list = prime_finder(int(math.sqrt(number+1)))
+    prime_list = prime_finder(math.floor(math.sqrt(number+1)))
     factor_list = factor_finder(prime_list, factor_list, number)
     end = time.time()
     print(end - start)
