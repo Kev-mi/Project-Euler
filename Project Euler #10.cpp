182e64ec526e1f12197821a7755083fbb90a8a3c
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2023-02-17 18:40:48 +0100

    Update Project Euler #10.cpp

--- Project Euler #10.cpp
+++ Project Euler #10.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
-#include <math.h> 
+#include <cmath>
 #include <vector>
-#include<ctime>
+#include <ctime>
 #include <numeric>
-using namespace std;
-std::vector<int> primelist = {2};
 
-void prime_finder(int number){
-    int prime[number+1] ={};
-    std::fill_n(prime, number+1, 1);
-    for (int y = 3;y < sqrt(number+1);y+= 2){
-        if (prime[y] == 1){
-            for (int x = y*y;x < number+1; x += y+y){
-                prime[x] = 0;}}}
-    for (int x=3; x < number+1; x+=2){
-        if (prime[x] == true){
-            primelist.push_back(x);}}
+long long prime_finder(int number) {
+    std::vector<int> primelist = { 2 };
+    int* prime = new int[number + 1] {};
+    std::fill_n(prime, number + 1, 1);
+    for (int y = 3; y < std::sqrt(number + 1); y += 2) {
+        if (prime[y] == 1) {
+            for (int x = y * y; x < number + 1; x += y + y) {
+                prime[x] = 0;
+            }
+        }
+    }
+    for (int x = 3; x < number + 1; x += 2) {
+        if (prime[x] == true) {
+            primelist.push_back(x);
+        }
+    }
     long long sum = std::accumulate(primelist.begin(), primelist.end(), 0LL);
-    cout << sum << endl;
-}
+    return sum;}
+
 
 int main()
 {
     int upper_bound = 1999999;
-    int start_s=clock() ;
-    prime_finder(upper_bound);
+    int start_s = clock();
+    long long sum = prime_finder(upper_bound);
+    std::cout << sum << std::endl;
     int stop_s = clock();
-    cout << (stop_s - start_s) / double(CLOCKS_PER_SEC);
-    return 0;}
+    std::cout << (stop_s - start_s) / double(CLOCKS_PER_SEC);
+    return 0;
+}
